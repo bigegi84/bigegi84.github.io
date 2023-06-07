cab216f8c1b590ef15737c026800064541abf7f7
bigegi84 <gilangpw84@gmail.com>
2023-06-07 20:43:57 +0700

    up

--- app/piano/song.js
+++ app/piano/song.js
@@ -1,7 +1,7 @@
 const song = {
   "Mahalini - Sisa Rasa Ritme": [
     "#Gb-2 #Gb-2 #Bbm-2 #Bbm-2 #Dbm-2 #Eb-2 #Abm-2 #Abm-2 #Bm-2 #Bm-2 #Gb-2 #Ebm-2 #Abm-2 #Ab-2 #Db-2 #Db-2 #Gb-2 #Gb-2 #Bbm-2 #Bbm-2 #Dbm-2 #Eb-2 #Abm-2 #Abm-2 #Bm-2 #Bm-2 #Gb-2 #Ebm-2 #Abm-2 #Ab-2 #Db-2 #Db-2 #Gb-2 #Gb-2 #Bbm-2 #Bbm-2 #E-2 #Eb-2 #Abm-2 #Abm-2 #Bm-2 #Bm-2 #Gb-2 #Ebm-2 #Abm-2 #Db-2 #Gb-1 #Gb-0.25 #B-0.75 #Gb-2 #Gb-2 #Gb-2 #Bbm-2 #Bbm-2 #Dbm-2 #Eb-2 #Abm-2 #Abm-2 #Bm-2 #Bm-2 #Gb-2 #Ebm-2 #Abm-2 #Ab-2 #Db-2 #Db-2 #Gb-2 #Gb-2 #Bbm-2 #Bbm-2 #E-2 #Eb-2 #Abm-2 #Abm-2 #Bm-2 #Bm-2 #Gb-2 #Ebm-2 #Abm-2 #Db-2 #B-1 #B-1 #B-1 #B-1 #Bbm-1 #Bbm-1 #Ebm-1 #Ebm-1 #Abm-1 #Abm-1 #Db-1 #Db-1 #Gb-1 #Gb-1 #E-1 #Db-1 #B-1 #B-1 #B-1 #B-1 #Bbm-1 #Bbm-0.5 #A-1.5 #A-1 #D-1 #D-1 #D-1 #D-1 #Db-1 #Db-1 #Db-1 #Db-1 #Gb-1 #Gb-1 #Gb-1 #Gb-1 #Bbm-1 #Bbm-1 #Bbm-1 #Bbm-1 #E-1 #E-1 #Eb-1 #Eb-1 #Abm-1 #Abm-1 #Abm-1 #Abm-1 #Bm-1 #Bm-1 #Bm-1 #Bm-1 #Gb-1 #Gb-1 #Ebm-1 #Ebm-1 #Abm-1 #Abm-1 #Db-1 #Db-1 #Ebm-1 #Ebm-1 #Ab-1 #Ab-1 #Ab-1 #Ab-1 #Abm-2 #Db-2 #Gb-2",
-    "Db5-0.5 B4-0.25 Bb4-0.5 B4-0.5 Db5-1.75 Bb4-0.25 B4-0.25 Db5-0.5 B4-0.25 Bb4-0.5 Eb5-0.5 Db5-1.75 Bb4-0.25 B4-0.25 Db5-0.5 Db5-0.25 Db5-0.5 Eb5-0.75 Db5-0.5 B4-0.25 B4-0.5 Bb4-0.5 B4-0.5",
+    "Db5-0.5 B4-0.25 Bb4-0.5 B4-0.5 Db5-1.75 Bb4-0.25 B4-0.25 Db5-0.5 B4-0.25 Bb4-0.5 Eb5-0.5 Db5-1.75 Bb4-0.25 B4-0.25 Db5-0.5 Db5-0.25 Db5-0.5 Eb5-0.75 Db5-0.5 B4-0.25 B4-0.5 Bb4-0.5 B4-3.75 Ab4-0.25 Bb4-0.25 B4-0.5 B4-0.25 B4-0.5",
   ],
   "Wage Rudolf Supratman - Ibu Kita Kartini Melodi": [
     "",

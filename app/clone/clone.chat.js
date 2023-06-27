0acc28632e68065dcd1bdaec967df74cbe217143
bigegi84 <gilangpw84@gmail.com>
2023-06-27 11:44:14 +0700

    up

--- app/clone/clone.chat.js
+++ app/clone/clone.chat.js
@@ -42,17 +42,68 @@ const cloneChat = {
           className={bigegi84theme.class.button}
           onClick={() => {
             const brain = cloneStore.brain[cloneStore.brain.selected];
-            const yaml = jsyaml.dump(mobx.toJS(brain));
+            const convert = (arr) => {
+              const obj = {};
+              arr.forEach(([word, child, $answer]) => {
+                const ch = child ? convert(child) : {};
+                obj[word] = {
+                  ...ch,
+                  ...{ $answer: $answer ? $answer : null },
+                };
+              });
+              return obj;
+            };
+            const findKey = (obj, arr) => {
+              let i = 0;
+              let str = [];
+              for (const key in obj) {
+                if (arr[0] == i) {
+                  str.push(key);
+                  str = [
+                    ...str,
+                    ...(arr[1] !== null
+                      ? findKey(obj[key], arr.splice(1))
+                      : []),
+                  ];
+                }
+                i++;
+              }
+              console.log(obj);
+              console.log(arr);
+              console.log(str);
+              return str;
+            };
+            let newBrain = convert(mobx.toJS(brain));
+            const convertAnswer = (obj) => {
+              for (const key in obj) {
+                // console.log(key);
+                if (key != "$answer") {
+                  const { $answer } = obj[key];
+                  obj[key] = {
+                    ...convertAnswer(obj[key]),
+                    ...($answer
+                      ? { $answer: [findKey(newBrain, $answer)] }
+                      : {}),
+                  };
+                }
+              }
+              return obj;
+            };
+            newBrain = convertAnswer(newBrain);
+            const yaml = jsyaml.dump(newBrain);
             const dataStr =
               "data:text/yaml;charset=utf-8," +
               encodeURIComponent(yaml);
             const dlAnchorElem = document.getElementById("downloadA");
             dlAnchorElem.setAttribute("href", dataStr);
-            dlAnchorElem.setAttribute("download", "bigegi84.bigegi84-Clone.yaml");
+            dlAnchorElem.setAttribute(
+              "download",
+              "bigegi84.bigegi84-Clone.yaml"
+            );
             dlAnchorElem.click();
           }}
         >
-          Simpan yaml
+          Simpan yaml ss
         </button>
         <a id="downloadA" style={{ display: "none" }}></a>
         {/* <strong>Jumlah kata yang dipelajari: </strong>

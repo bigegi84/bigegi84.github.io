0b15052188db6281039fed872fd4d98fe1893cec
BIGEGI84-LM91P\gilan <gilangpw84@outlook.com>
2024-07-30 15:59:33 +0700

    up

--- app/chordAdmin/chordAdmin.state.js
+++ app/chordAdmin/chordAdmin.state.js
@@ -1,6 +1,6 @@
 const chordAdminState = {
   // apiUrl: "http://localhost:8000/api/chord",
   // apiUrl: "http://localhost:841/chord",
-  apiUrl: "http://bigegi84.kencang.id/bigegi84-api-lumen/index.php/chord",
+  apiUrl: "https://bigegi84.kencang.id/bigegi84-api-lumen/index.php/chord",
   apiToken: localStorage.getItem("chordAdmin-apiToken"),
 };

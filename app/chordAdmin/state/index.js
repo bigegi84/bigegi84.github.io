9764a7338aef47ae5af3f88be1f25268a4567464
Gilang Pratama Wiguna <gilangpw84@outlook.com>
2024-08-07 15:36:19 +0700

    up

---
+++ app/chordAdmin/state/index.js
@@ -0,0 +1,8 @@
+define(() => {
+  return {
+    // apiUrl: "http://localhost:8000/api/chord",
+    // apiUrl: "http://localhost:841/chord",
+    apiUrl: "https://bigegi84.kencang.id/bigegi84-api-lumen/index.php/chord",
+    apiToken: localStorage.getItem("chordAdmin-apiToken"),
+  };
+});

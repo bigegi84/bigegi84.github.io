9c6058e2f1e10366b1e059424df7f8b4fb396ccf
Gilang Pratama Wiguna <GilangPW@ag-it.com>
2024-08-13 15:18:05 +0700

    up

--- app/c/chordAdmin/view.js
+++ app/c/chordAdmin/view.js
@@ -2,8 +2,6 @@ define((require) => {
   var { pakuan } = require("../../../lib/index");
   var { login, song } = require("./page/index");
   var state = require("./state/index");
-  var store = require("./store/index");
-  console.log(store);
   return () =>
     pakuan.main({
       textHighlight: "bigegi84 - Chord Admin",
